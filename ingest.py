bd9fd46769eb4604f00aa860d04413e0f7914730
Gaurav <gaurav.lamichhane02@gmail.com>
2026-03-31 22:29:16 +0545

    refactor: use hugging face embeddings

--- ingest.py
+++ ingest.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader, TextLoader, WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 load_dotenv()
 
